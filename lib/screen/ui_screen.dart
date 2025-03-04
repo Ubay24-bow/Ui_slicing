aaca53fc9a6f468b5be1eee95956ba4adc20a163
Ubay24-bow <ubaydillah.tech@gmail.com>
2025-03-04 12:58:20 +0700

    Update ui_screen.dart

--- lib/screen/ui_screen.dart
+++ lib/screen/ui_screen.dart
@@ -23,10 +23,10 @@ class _UiScreenState extends State<UiScreen> {
       body: SafeArea(
         child: Column(
           children: [
-            // This will push the center content to the middle
+            
             const Spacer(),
             
-            // Center content
+          
             Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -50,10 +50,10 @@ class _UiScreenState extends State<UiScreen> {
               ),
             ),
             
-            // This will push the bottom content to the bottom
+         
             const Spacer(),
             
-            // Bottom logo
+        
             Padding(
               padding: const EdgeInsets.only(bottom: 16.0),
               child: Column(
